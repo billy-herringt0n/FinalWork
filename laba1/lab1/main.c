6b198cf5fe65ddba32c6de2c8a9232749b1103a1
Aleks <Aleksnikmaster@gmail.com>
2023-05-16 21:49:44 +0300

    Changed progr1

--- laba1/lab1/main.c
+++ laba1/lab1/main.c
@@ -3,16 +3,24 @@
 #include <locale.h>
 
 void main() {
-	const double k1 = 2.0, k2 = 1.0, k3 = 10;
-	double x, y, ln, tg, a, b, c;
+	const double k1 = 2.0;
+	const double k2 = 1.0;
+	const double k3 = 10;
+	double x;
+	double y;
+	double ln; // переменная для вычисления логарифма
+	double tg; 
+	double eax;
+	double b; 
+	double c;
 	setlocale(LC_ALL, ".1251");
 	printf("Введите число x\n");
 	scanf_s("%lf", &x);
 	ln = log(x);
-	a = (k1 + x) / pow(x, 2);
+	eax = (k1 + x) / pow(x, 2);
 	tg = tan(x);
 	b = sqrt(pow(x, 2) + k3);
 	c = k2 / b;
-	y = (ln + a) / (tg - c);
+	y = (ln + eax) / (tg - c);
 	printf("Искомое значение y равно - %lf", y);
 }
