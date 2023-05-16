03fbdc85ef1fa02ea059a706c5ab217e9cb4a07f
Ярослав <kazymysik000@mail.ru>
2023-05-16 21:20:49 +0300

    Add new 1odt file

--- laba1/lab1/main.c
+++ laba1/lab1/main.c
@@ -4,15 +4,15 @@
 
 void main() {
 	const double k1 = 2.0, k2 = 1.0, k3 = 10;
-	double x, y, ln, tg, eax, b, c;
+	double x, y, ln, tg, a, b, c;
 	setlocale(LC_ALL, ".1251");
 	printf("Введите число x\n");
 	scanf_s("%lf", &x);
 	ln = log(x);
-	eax = (k1 + x) / pow(x, 2);
+	a = (k1 + x) / pow(x, 2);
 	tg = tan(x);
 	b = sqrt(pow(x, 2) + k3);
 	c = k2 / b;
-	y = (ln + eax) / (tg - c);
+	y = (ln + a) / (tg - c);
 	printf("Искомое значение y равно - %lf", y);
 }
